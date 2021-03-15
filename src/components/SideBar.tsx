3a49e346cf80942d318826e3d374e0de51463827
Leonardo Antonio Bonfim Macedo <leonardo.macedo@grendene.com.br>
2021-03-15 20:12:15 -0300

    Desafio completo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,50 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+
+import { Button } from './Button';
+
+export interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genreId: number;
+  setGenreId: Function;
+}
+
+export function SideBar({ genreId, setGenreId}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={genre.id}
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={genreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
